a66e129a3ca1c691f1b1242591ced2b52019c5e1
mauri-rivera <mauri.rivera@outlook.com>
2023-10-29 19:20:32 -0300

    update3

---
+++ TalSec/Controllers/HomeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using TalSec.Models;
+
+namespace TalSec.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public static string? nombreUsuario;
+    public static int valorNumerico = 22;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        HttpContext.Session.SetString("Nombre", "Mauricio");
+        nombreUsuario = HttpContext.Session.GetString("Nombre");
+
+        if (nombreUsuario == null)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    [HttpPost("")]
+    public IActionResult Index(Usuario usuario)
+    {
+        if (ModelState.IsValid && nombreUsuario == usuario.Nombre)
+        {
+            return RedirectToAction("Dashboard", usuario);
+        }
+        else
+        {
+            return View();
+        }
+    }
+
+    [HttpGet("dashboard")]
+    public IActionResult Dashboard()
+    {
+        ViewBag.Numero = valorNumerico;
+
+        return View();
+    }
+
+    [HttpPost("dashboard")]
+    public IActionResult Dashboard(Usuario usuario, string numero)
+    {
+        if (numero == "+Uno")
+        {
+            valorNumerico = usuario.SumarUno(valorNumerico);
+        }
+        else if (numero == "-Uno")
+        {
+            valorNumerico = usuario.RestarUno(valorNumerico);
+        }
+        else if (numero == "xDos")
+        {
+            valorNumerico = usuario.MultiplicarDos(valorNumerico);
+        }
+        else if (numero == "Random")
+        {
+            valorNumerico = usuario.SumaAleatoria(valorNumerico);
+        }
+
+        ViewBag.Numero = valorNumerico;
+
+        return View();
+    }
+
+    public IActionResult LogOut()
+    {
+        valorNumerico = 22;
+
+        HttpContext.Session.Clear();
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ TalSec/Models/Usuario.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TalSec.Models;
+
+public class Usuario
+{
+    [Required(ErrorMessage = "El nombre de usuario es requerido")]
+    public string Nombre { get; set; } = "";
+
+    public int SumarUno(int numero)
+    {
+        int resultado;
+
+        numero = numero + 1;
+        resultado = numero;
+
+        return resultado;
+    }
+
+    public int RestarUno(int numero)
+    {
+        int resultado;
+
+        numero = numero - 1;
+        resultado = numero;
+
+        return resultado;
+    }
+
+    public int MultiplicarDos(int numero)
+    {
+        int resultado = numero * 2;
+
+        return resultado;
+    }
+
+    public int SumaAleatoria(int numero)
+    {
+        Random rangoNumero = new();
+        int aleatorio = rangoNumero.Next(1, 11);
+
+        int resultado = aleatorio + numero;
+
+        return resultado;
+    }
+}
+
+
